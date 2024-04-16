a42dc212808d57ea317f974b9ca87deeca15bd36
Steven Moser <mellotronplayer@gmail.com>
2024-04-16 14:07:19 -0700

    Update basics.cpp

--- src/Ch01-The Basics of Exception Handling/01_06/begin/basics.cpp
+++ src/Ch01-The Basics of Exception Handling/01_06/begin/basics.cpp
@@ -7,10 +7,6 @@ using namespace std;
 const string readConfigFile(const string& filename) 
 {
     ifstream file(filename);
-    if (!file.good()) 
-    {
-        throw runtime_error("Failed to open configuration file.");
-    }
 
     string line;
     string text;
